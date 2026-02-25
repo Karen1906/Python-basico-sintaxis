b9dadfab5ef203e384a4842b23ae4f313550baa8
Karen1906 <nayes9667@gmail.com>
2026-02-25 12:23:02 -0500

    first commit

---
+++ 1-tipos_de_datos.py
@@ -0,0 +1,23 @@
+#tipos de datos en Python
+#Nayely Karen Salas Mayta
+
+#1. tipo de dato numérico (int)
+edad = 25
+promedio = 19 
+numero = -50 
+
+#2. tipo de dato  decimal (float)
+peso = 75.50
+estatura = 170.30
+temperatura = 14.70 
+precio = 99.99
+
+#3. tipo de dato texto (str)
+letra = 'A'
+nombre = 'Albert'
+apellido = "Nina"
+dni = 73855806
+
+#4. tipo de dato booleanos (bool)
+es_mayor_de_edad = True
+puede_volar = False
---
+++ 2-operadores.py
@@ -0,0 +1,45 @@
+#Operadores en python
+#Nayely Karen Salas Mayta
+
+#1. Operadores Aritmeticos
+suma = 5 + 5
+resta = 10 - 6 
+multiplicacion = 5 * 5 
+division = 10 / 2
+modulo = 10 % 2
+exponente = 10 ** 2
+
+print('El resultado de la suma es:',suma)
+print(resta)
+print(multiplicacion)
+print(division)
+print(modulo)
+print(exponente)
+
+#2. Operadores de comparación
+print( 5 == 5 )  #igual a
+print( 5 != 5 )  #Diferente de 
+print( 10 > 5 )  #Mayor que
+print( 10 < 5 )  #Menor que 
+print( 10 >= 5 ) #Mayor igual que 
+print( 10 <= 5 ) #Menor igual que 
+
+#3. operadores logicos 
+v = True
+f = False
+ #3.1. and (y)
+print('-----------------------------------AND')
+print(v and v)
+print(v and f)
+print(f and v)
+print(f and f)
+ #3.2. or (o)
+print('------------------------------------OR')
+print(v and v)
+print(v and f)
+print(f and v)
+print(f and f)
+ #3.3. not (negation)
+print('------------------------------------NOT')
+print(not v)
+print(not f)
